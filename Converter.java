f2b09665f1886faeb9659746289aae5479c725a4
Cskirby34 <cmking3388@gmail.com>
2026-03-28 14:36:51 -0400

    Update and rename converter.java to Converter.java

---
+++ Converter.java
@@ -0,0 +1,16 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public abstract class Converter {
+  public void convert(String inputFileName, String outputFileName) throw IOException{
+    BufferedImage inputImage = ImageIO.read(new File(inputFileName));
+
+    BufferedImage outputImage = processImage(inputImage);
+
+    ImageIO.write(outputImage, "PNG", new File(outputFileName));
+  }
+  
+  
+}
