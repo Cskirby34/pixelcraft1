581918d2683594e4a1445b547b3cdc9c3bb31d02
NinjaNathan1234567 <157557730+NinjaNathan1234567@users.noreply.github.com>
2026-03-29 18:05:03 -0400

    Update Invert.java

--- Invert.java
+++ Invert.java
@@ -9,3 +9,15 @@ public class Invert extends Converter {
         invertRows(image, result, width, height, 0);
         return result;
     }
+    private void invertrows(BufferedImage src, BufferedImage dst, int width, int height, int y) {
+       if (y >= height) return;
+        invertColumns(src, dst, width, y, 0);
+        invertRows(src, dst, width, height, y + 1);
+    }
+    private void invertColumns(BufferedImage src, BufferedImage dst, int width, int y, int x) {
+        if (x >= width) return;
+           ARGB p = getPixel(src, x, y);
+           setPixel(dst, x, y, new ARGB(p.alpha, 255 - p.red, 255 - p.green, 255 - p.blue));
+           invertColumns(src, dst, width, y, x + 1);
+    }
+}
