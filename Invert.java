1778b99fae6c02da72b73c7a2bc18fec586b8c72
NinjaNathan1234567 <157557730+NinjaNathan1234567@users.noreply.github.com>
2026-03-29 18:02:25 -0400

    Update Invert.java

--- Invert.java
+++ Invert.java
@@ -1,1 +1,11 @@
+import java.awt.image.BufferedImage;
 
+public class Invert extends Converter {
+   @Override
+    protected BufferedImage processImage(BufferedImage image) {
+        int width  = getWidth(image);
+        int height = getHeight(image);
+        BufferedImage result = createBlankImage(width, height);
+        invertRows(image, result, width, height, 0);
+        return result;
+    }
