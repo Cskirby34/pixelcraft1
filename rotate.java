f8c7a33dd533619ded221ce1a1a30b8c9bb17694
NinjaNathan1234567 <157557730+NinjaNathan1234567@users.noreply.github.com>
2026-03-29 20:52:24 -0400

    Update rotate.java

--- rotate.java
+++ rotate.java
@@ -1,24 +1,19 @@
 import java.awt.image.BufferedImage;
 
 public class Rotate extends Converter {
-  @Override
-  protected BufferedImage processImage(BufferedImage image) {
-    int scrWidth=getWidth(image);
-    int scrHeight=getHeight(image);
-    BufferedImage result = createBlankImage(srcHeight, srcWidth);
 
-    for (int y = 0; y < srcHeight; y++) {
+    @Override
+    protected BufferedImage processImage(BufferedImage input) {
+        int srcWidth = input.getWidth();
+        int srcHeight = input.getHeight();
+        BufferedImage output = new BufferedImage(srcHeight, srcWidth, BufferedImage.TYPE_INT_ARGB);
+
+        for (int y = 0; y < srcHeight; y++) {
             for (int x = 0; x < srcWidth; x++) {
-                ARGB pixel = getPixel(image, x, y);
-                setPixel(result, srcHeight - 1 - y, x, pixel);
+                ARGB pixel = new ARGB(input.getRGB(x, y));
+                output.setRGB(srcHeight - 1 - y, x, pixel.toInt());
             }
         }
-        return result;
+        return output;
     }
 }
-
-
-
- 
-
-  
