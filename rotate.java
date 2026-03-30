1491fa77c7985d04524305c4a506cf2ebccc36aa
NinjaNathan1234567 <157557730+NinjaNathan1234567@users.noreply.github.com>
2026-03-29 20:42:36 -0400

    Update rotate.java

--- rotate.java
+++ rotate.java
@@ -10,7 +10,6 @@ public class Rotate extends Converter {
     for (int y = 0; y < srcHeight; y++) {
             for (int x = 0; x < srcWidth; x++) {
                 ARGB pixel = getPixel(image, x, y);
-                // 90° CW: new_x = (srcHeight - 1 - y), new_y = x
                 setPixel(result, srcHeight - 1 - y, x, pixel);
             }
         }
